6d322f69272ee398c83983530f6096ae1a15662d
Ensar YAZICI <ensar.yaz@gmail.com>
2022-10-01 17:02:08 +0300

    components

---
+++ MealOrdering/MealOrdering/Client/Utils/ModalManager.cs
@@ -0,0 +1,43 @@
+﻿using Blazored.Modal;
+using Blazored.Modal.Services;
+using MealOrdering.Client.CustomComponents.ModalComponents;
+
+namespace MealOrdering.Client.Utils
+{
+    public class ModalManager
+    {
+        private readonly IModalService modalService;
+
+        public ModalManager(IModalService ModalService)
+        {
+            modalService = ModalService;
+        }
+
+        public async Task ShowMessageAsync(String Title, String Message, int Duration = 0)
+        {
+            ModalParameters mParams = new ModalParameters();
+            mParams.Add("Message", Message);
+
+            var modalRef = modalService.Show<ShowMessagePopupComponent>(Title, mParams);
+
+            if (Duration > 0)
+            {
+                await Task.Delay(Duration);
+                modalRef.Close();
+            }
+        }
+
+        public async Task<bool> ConfirmationAsync(String Title, String Message)
+        {
+            ModalParameters mParams = new ModalParameters();
+            mParams.Add("Message", Message);
+
+            var modalRef = modalService.Show<ConfirmationPopupComponent>(Title, mParams);
+            var modalResult = await modalRef.Result;
+
+            return !modalResult.Cancelled;
+        }
+
+    }
+
+}
--- MealOrdering/MealOrdering/Server/Program.cs
+++ MealOrdering/MealOrdering/Server/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Modal;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddBlazoredModal();
+
 
 var app = builder.Build();
 
